e92051f8426a6b14d15e2f8cba82b3477b33af5e
EstebanRey <estebanbraianrey@gmail.com>
2022-08-15 21:46:16 -0300

    Actualizacion de vista agregar y editar productos

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,4 +1,15 @@
+const { request } = require("express")
+
 module.exports = {
+    add : (req,res) => {
+        return res.render ('productAdd')
+    },
+    store : (req,res) => {
+        return res.send (req.body)
+    },
+    edit : (req,res) =>{
+        return res.render ('productEdit')
+    },
     productos: (req,res) => {
         return res.render ('productos')
     },
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const {productos,detalle,carrito} = require('../controllers/productosController')
+const {productos,detalle,carrito,add,store,edit} = require('../controllers/productosController')
+
+
+
 /* GET users listing. */
+router.get('/add',add)
+router.post('/add',store)
+router.get('/edit',edit)
 router.get('/productos',productos)
 router.get('/detalle', detalle)
 router.get('/carrito',carrito)
