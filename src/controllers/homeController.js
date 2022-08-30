75aead13b305de2e00fde56431b781953b0d2b79
LucasJavierBarros <lucasbarros78@hotmail.com>
2022-08-29 22:37:54 -0300

    reparado carrito

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,9 @@
 const categorias = require('../data/categorias.json')
-const productos = require('../data/productos.json')
 const { loadCarrito, loadProduct, storeCarrito, storeProducts } = require('../data/db_Module')
 
 module.exports = {
     home: (req, res) => {
-        const producto = loadProduct();
+        const productos = loadProduct();
         const carrito = loadCarrito();
         return res.render('home', {
             productos,
