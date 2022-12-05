669443189186a5a83b6e33733c5138ccc30ac4da
EstebanRey <estebanbraianrey@gmail.com>
2022-12-05 20:38:58 -0300

    subo paracial api

--- src/routes/API/apiUsers.js
+++ src/routes/API/apiUsers.js
@@ -1,13 +1,22 @@
-var express = require('express');
-var router = express.Router();
-
-const { verifyEmail } = require('../../controllers/API/apiUser');
+const express = require('express');
+const router = express.Router();
+const { upload } = require('../../middleware/uploadFiles')
+const { login, registro, procesoRegistro, procesoLogin, perfil, logout, update, verifyEmail } = require('../../controllers/API/apiUsers')
+const { editarProductosValidaciones, agregarProductoValidaciones } = require('../../validaciones')
 
 
 
 /* GET users listing. */
 
 router
+    .get('/registro', registro)
+    .post('/registro', uploadUser.single('imagenUser'), registroValidacion, procesoRegistro)
+    .get('/login', login)
+    .post('/login', loginValidacion, procesoLogin)
+    .get('/perfil', userSessionCheck, perfil)
+    .get('/logout', logout)
+    .put('/update/:id', uploadUser.single('imagenUser'), registroValidacion, update)
+
     .post('/verified', verifyEmail) //agregado ruta para validar el email
 
 
