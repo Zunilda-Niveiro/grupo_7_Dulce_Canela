a7cc3f6e894b497e19c4e80ff175b92f59e7dee2
LucasJavierBarros <lucasbarros78@hotmail.com>
2022-12-22 17:46:46 -0300

    agregado icono y texto admin

--- admin/src/pages/Users/UserCard/UserCard.jsx
+++ admin/src/pages/Users/UserCard/UserCard.jsx
@@ -6,7 +6,7 @@ import './UserCard.css'
 export const UserCard = ({user ,idUser}) => {
 
 const styles = {
-    border: '5px solid gold', 
+    border: '5px solid #43b2e2', 
 };
 
   return (
@@ -15,6 +15,7 @@ const styles = {
             <div className="blob" ></div>
             <span className="img" style={{ backgroundImage: `url('${user.url ==='http://localhost:4000/api/users/images/' ? 'http://localhost:4000/api/users/images/userDefault.png' :user.url }')` }}></span>
             <h2>{`${user.firstname} ${user.surname}`}</h2>
+            <h5>{user.rol_id === 2 ? <i class="fa-solid fa-user-gear">  ADMINISTRADOR</i> : null}</h5>
             <p>
                 <i className="fas fa-edit" onClick={()=>idUser(user.id)}></i>
                 <i className="fas fa-trash-alt"></i>
